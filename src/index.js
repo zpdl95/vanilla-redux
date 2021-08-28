96e618e627a823f15323f408d9c0a451259f1da1
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2021-08-28 21:03:56 +0900

    #1.1 Store and Reducer

--- src/index.js
+++ src/index.js
@@ -1,24 +1,14 @@
+import { createStore } from "redux";
+
 const plus = document.getElementById("plus");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
+/* 함수가 받는 인자값설정으로 기초값을 설정함 */
+/* 데이터의 수정은 이 함수안에서 이루어 져야함 */
+const countModifier = (state = 0) => {
+  return state;
 };
-
-const handlePlus = () => {
-  count = count + 1;
-  updateText();
-};
-
-const handleMinus = () => {
-  count = count - 1;
-  updateText();
-};
-
-plus.addEventListener("click", handlePlus);
-minus.addEventListener("click", handleMinus);
+/* store은 state와 같이 데이터를 저장하고 수정하는 함수다 */
+/* createStore함수는 데이터를 수정하는 함수를 인자값으로 받는자 */
+const countStore = createStore(countModifier);
