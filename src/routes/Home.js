e2ba8195e0605f5fc3eff7402451df75c08d49aa
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2021-08-30 23:58:01 +0900

    #3.0 Setup

---
+++ src/routes/Home.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+/*<></>는 여러게의 element를 return 할때 쓸 수 있는 투명한 div와 같다 */
+/* onChange는 input안의 값이 변경될때 실행 */
+function Home() {
+  const [text, setText] = useState("");
+  function onChange(event) {
+    setText(event.target.value);
+  }
+  function onSubmit(event) {
+    event.preventDefault();
+    console.log(text);
+  }
+  return (
+    <>
+      <h1>To Do</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={text} onChange={onChange} />
+        <ul></ul>
+        <button>Add</button>
+      </form>
+    </>
+  );
+}
+
+export default Home;
